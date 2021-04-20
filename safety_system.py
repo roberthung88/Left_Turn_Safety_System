ca7890c26d43a2b11aa21c69e02e1ef06f40b705
Gerardo Flores-Galvez <GerardoFlores@macbook-pro-6.lan>
2021-04-19 19:49:10 -0700

    second attempt. last one was empty

---
+++ safety_system.py
@@ -0,0 +1,10 @@
+def safety_system(dist, vfoot)
+
+    # Time of Arrival value calculated by the distance and velocity 
+    toa = dist / vfoot 
+
+    # if logic to determine if it safe to turn based on the time of arrival. Assuming 7 seconds for a safe turn.
+    if toa > 7: 
+        print("Go. Safe to turn.")
+    if toa <= 7:
+        print("Stop. Not safe to turn.")
