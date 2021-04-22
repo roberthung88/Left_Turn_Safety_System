c7eb3134b11852364ecf8684ea2aa6bfe73ca165
Gerardo Flores-Galvez <GerardoFlores@macbook-pro-6.lan>
2021-04-21 20:37:40 -0700

    this commit is using the resolution of the sample videos: 720p

--- data_collection.py
+++ data_collection.py
@@ -3,22 +3,39 @@ def data_collection(startY, endY):
     ypix = (endY - startY)
 
     # pixel tiers accounting for road depth in ft/pixel
-    tfive = 60/264 
-    tfour = 60/400
-    tthree = 60/500
-    ttwo = 60/600
-    tone = 60/700
+    #tfive = 60/264 
+    #tfour = 60/400
+    #tthree = 60/500
+    #ttwo = 60/600
+    #tone = 60/700
+
+    tfive = 40/100 
+    tfour = 40/120
+    tthree = 40/140
+    ttwo = 40/160
+    tone = 40/180
 
     # tier is determined by the endY value instead of the startY value to be conservative
-    if endY in range(0,700):
+    #if endY in range(0,700):
+     #   yfoot = ypix * tone
+    #if endY in range(700, 1300):
+     #   yfoot = ypix * ttwo
+    #if endY in range(1300, 1800):
+     #   yfoot = ypix * tthree
+    #if endY in range(1800, 2200):
+     #   yfoot = ypix * tfour
+    #if endY in range(2200, 2464):
+     #   yfoot = ypix * tfive
+
+    if endY in range(0,180):
         yfoot = ypix * tone
-    if endY in range(700, 1300):
+    if endY in range(180, 340):
         yfoot = ypix * ttwo
-    if endY in range(1300, 1800):
+    if endY in range(340, 480):
         yfoot = ypix * tthree
-    if endY in range(1800, 2200):
+    if endY in range(480, 600):
         yfoot = ypix * tfour
-    if endY in range(2200, 2464):
+    if endY in range(600, 700):
         yfoot = ypix * tfive
 
     # velocity of vehicle in between detections
