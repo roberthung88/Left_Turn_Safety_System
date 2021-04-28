1a7229e6af413fde03b1c467ee18b32a1a8a6c51
weixuan <weixuang@usc.edu>
2021-04-28 23:27:40 +0800

    bm stereo matching

--- distance_detection/distance_detection.py
+++ distance_detection/distance_detection.py
@@ -6,58 +6,47 @@ from jetcam.csi_camera import CSICamera
 camera_right = CSICamera(capture_device=0, width=1280, height=720)
 camera_left = CSICamera(capture_device=1, width=1280, height=720)
 
-#SGBM parameters
-# mindisparity = 32
-# ndisparities = 176
-# SADWindowsSize = 16
-# P1 = 4 * 1 * SADWindowsSize
-# P2 = 32 * 1 * SADWindowsSize
+cv2.namedWindow("depth")
+def callbackFunc(e, x, y, f, p):
+    if e == cv2.EVENT_LBUTTONDOWN:
+        print (threeD[y][x][2])
 
-# sgbm = cv2.StereoSGBM_create(mindisparity, ndisparities, SADWindowsSize)
-# sgbm.setP1(P1)
-# sgbm.setP2(P2)
-# sgbm.setPreFilterCap(60)
-# sgbm.setUniquenessRatio(30)
-# sgbm.setSpeckleRange(2)
-# sgbm.setSpeckleWindowSize(200)
-# sgbm.setDisp12MaxDiff(1)
-# disp sgbm.compute()
+cv2.setMouseCallback("depth", callbackFunc, None)
 
-image_right = camera_right.read()
-image_left = camera_left.read()
-# cv2.imshow("right",image_right)
-# cv2.imshow("left",image_left)
+cv2.createTrackbar("num","depth",2,10, lambda x: None)
+cv2.createTrackbar("blockSize","depth",5,255,lambda x:None)
 
-image_right_rectify = cv2.remap(image_right, cam_config.right_map1, cam_config.right_map2, cv2.INTER_LINEAR)
-image_left_rectify = cv2.remap(image_left, cam_config.left_map1, cam_config.left_map2, cv2.INTER_LINEAR)
-# cv2.imshow("right_rectify",image_right_rectify)
-# cv2.imshow("left_rectify",image_left_rectify)
+while True:
+    image_right = camera_right.read()
+    image_left = camera_left.read()
+    # cv2.imshow("right",image_right)
+    # cv2.imshow("left",image_left)
+    image_right_rectify = cv2.remap(image_right, cam_config.right_map1, cam_config.right_map2, cv2.INTER_LINEAR)
+    image_left_rectify = cv2.remap(image_left, cam_config.left_map1, cam_config.left_map2, cv2.INTER_LINEAR)
+    # cv2.imshow("right_rectify",image_right_rectify)
+    # cv2.imshow("left_rectify",image_left_rectify)
 
-# image_right_gs = cv2.cvtColor(image_right, cv2.COLOR_BGR2GRAY)
-# image_left_gs = cv2.cvtColor(image_left, cv2.COLOR_BGR2GRAY)
+    image_right_gs = cv2.cvtColor(image_right_rectify, cv2.COLOR_BGR2GRAY)
+    image_left_gs = cv2.cvtColor(image_left_rectify, cv2.COLOR_BGR2GRAY)
 
-# image_right_gs_rectify = cv2.remap(image_right_gs, cam_config.right_map1, cam_config.right_map2, cv2.INTER_LINEAR)
-# image_left_gs_rectify = cv2.remap(image_left_gs, cam_config.left_map1, cam_config.left_map2, cv2.INTER_LINEAR)
+    num = cv2.getTrackbarPos("num", "depth")
+    blockSize = cv2.getTrackbarPos("blockSize","depth")
+    if blockSize % 2 == 0:
+        blockSize += 1
+    if blockSize < 5:
+        blockSize = 5
+    
+    stereo = cv2.StereoBM_create(numDisparities = 16*num, blockSize = 31)
+    disparity = stereo.compute(image_left_gs,image_right_gs)
+    disp = cv2.normalize(disparity, disparity, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    threeD = cv2.reprojectImageTo3D(disparity.astype(np.float32)/16., cam_config.Q)
 
-#disparity
-# disp = sgbm.compute(image_left_gs_rectify, image_right_gs_rectify)
-
-# xyz_coordinate = cv2.reprojectImageTo3D(disp, cam_config.Q, handleMissingValues = True)
-# xyz_coordinate = xyz_coordinate*16
-
-# disp = disp.astype(np.float32) / 16.0
-# disp8U = cv2.normalize(disp, disp, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype = cv2.CV_8U)
-# disp8U = cv2.medianBlur(disp8U, 9)
-
-def onMouse(event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-        print("point(%d, %d)" % (x, y))
-        cv2.imwrite("/home/nano/Desktop/demo/right_x.jpg",image_right_rectify)
-
-cv2.imshow("right_rectify", image_right_rectify)
-cv2.setMouseCallback("right_rectify", onMouse, 0)
-
-cv2.waitKey(0) # set time delay between each frame
-   
+    cv2.imshow("left_rectify",image_left_rectify)
+    cv2.imshow("depth",disp)
+    if cv2.waitKey(24):
+        continue
+    elif cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+        cv2.destroyAllWindows()
 
 cv2.destroyAllWindows()
